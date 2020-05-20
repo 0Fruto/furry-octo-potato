031b7edfda77e7f80bad9f7ccb71f92bdf42bbb0
fruto <maxmart999@icloud.com>
2020-05-20 16:25:19 +0300

    Ledder update(v0.0.0.2)
    -Ledders
    
    -Animation controller
    
    -Some test sprites
    
    -Controls are now a bit better

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -5,7 +5,8 @@ var slopeStop = 64
 var gravity = 1000.0
 export var walkSpeed = 250
 export var jumpVelocity = -400
-
+var isTochingLedder = false
+var climbing = false
 
 var velocity = Vector2()
 
@@ -13,17 +14,63 @@ func _ready():
 	pass 
 
 func _physics_process(delta):
+	if isTochingLedder:
+		if !is_on_floor() || Input.is_action_pressed("ui_up"):
+			climbing = true
+	
 	GetInput()
-	velocity.y += gravity * delta
-	
-	
+	$TOS.text = str(climbing)
+	if !climbing:
+		velocity.y += gravity * delta
 	velocity = move_and_slide(velocity, Vector2(0, -1), slopeStop)
+	ProcessAnimation()
+
+func ProcessAnimation():
+	if climbing:
+		if Input.is_action_pressed("ui_up"):
+			$Sprite.play("Climbing")
+		if Input.is_action_pressed("ui_down"):
+			$Sprite.play("Climbing")
+		if !is_on_floor():
+			if !Input.is_action_pressed("ui_up") && !Input.is_action_pressed("ui_down"):
+				$Sprite.play("Climbing")
+				$Sprite.stop()
+		else: if Input.is_action_pressed("ui_down"):
+			$Sprite.play("Idle")
+			climbing = false
+	else:
+		if !is_on_floor():
+			$Sprite.play("Jump")
+		else:
+			if !Input.is_action_pressed("ui_right") && !Input.is_action_pressed("ui_left"):
+				if Input.is_action_pressed("ui_down"):
+					$Sprite.play("Crouch")
+				else:
+					$Sprite.play("Idle")
+		
+		if Input.is_action_pressed("ui_right") || Input.is_action_pressed("ui_left"):
+			if is_on_floor():
+				$Sprite.play("Run")
+		else: if velocity.x == 0:
+			if Input.is_action_pressed("ui_down"):
+				$Sprite.play("Crouch")
+			else:
+				$Sprite.play("Idle")
 
 func GetInput():
 	var moveDirection = -int(Input.is_action_pressed("ui_left")) + int(Input.is_action_pressed("ui_right"))
 	velocity.x = lerp(velocity.x, walkSpeed * moveDirection, GetHWeight())
-	#if moveDirection != 0:
-	#	$Body.scale.x = moveDirection
+	if moveDirection != 0:
+		$Sprite.scale.x = moveDirection
+	if climbing:
+		if Input.is_action_pressed("ui_up"):
+			velocity.y = -100
+		else:
+			if Input.is_action_pressed("ui_down"):
+				velocity.y = 100
+			else:
+				velocity.y = 0
+		
 
 func GetHWeight():
 	return 0.2 if is_on_floor() else 0.1
@@ -31,3 +78,12 @@ func GetHWeight():
 func _input(event):
 	if event.is_action_pressed("jump") && is_on_floor():
 		velocity.y = jumpVelocity
+
+func _on_ledders_body_exited(_body):
+	isTochingLedder = false
+	climbing = false
+
+
+func _on_ledders_body_entered(_body):
+	isTochingLedder = true
+	velocity.y = 0
--- Scripts/Trail.gd
+++ Scripts/Trail.gd
@@ -5,7 +5,7 @@ export var length = 20
 func _ready():
 	pass
 
-func _process(delta):
+func _process(_delta):
 	global_position = Vector2(0,0)
 	add_point($"..".global_position)
 	while get_point_count() > length:
